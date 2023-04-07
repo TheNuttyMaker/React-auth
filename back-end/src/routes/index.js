0dd8e7572c9a3812d5ebfe3be041de6295f4e652
Prashant Yadav <pygold@gmail.com>
2023-04-08 01:23:08 +0530

    After login allow user to make changes to his profile (#3)

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,6 +2,7 @@ import { testRoute } from './testRoute';
 import { config } from 'dotenv';
 import { signUpRoute } from './signUpRoutes';
 import { LoginRoute } from './loginRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
 
 config();
 console.log(process.env.DB_URI);
@@ -9,5 +10,6 @@ console.log(process.env.DB_URI);
 export const routes = [
     testRoute,
     signUpRoute,
-    LoginRoute
+    LoginRoute,
+    updateUserInfoRoute,
 ];
--- front-end/src/pages/UserInfoPage.js
+++ front-end/src/pages/UserInfoPage.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useToken } from '../auth/useToken';
+import { useUser } from '../auth/useUser';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 export const UserInfoPage = () => {
+    const user = useUser();
+    const [token, setToken] = useToken();
+
+    const { id, email, info } = user;
     // We'll use the history to navigate the user
     // programmatically later on (we're not using it yet)
-    // const history = useHistory();
+    const history = useHistory();
 
     // These states are bound to the values of the text inputs
     // on the page (see JSX below). 
-    const [favoriteFood, setFavoriteFood] = useState('');
-    const [hairColor, setHairColor] = useState('');
-    const [bio, setBio] = useState('');
+    const [favoriteFood, setFavoriteFood] = useState(info.favoriteFood || '');
+    const [hairColor, setHairColor] = useState(info.hairColor || '');
+    const [bio, setBio] = useState(info.bio || '');
 
     // These state variables control whether or not we show
     // the success and error message sections after making
@@ -31,28 +38,48 @@ export const UserInfoPage = () => {
     }, [showSuccessMessage, showErrorMessage]);
 
     const saveChanges = async () => {
+        try {
+            const response = await axios.put(`/api/users/${id}`, {
+                favoriteFood,
+                hairColor,
+                bio,
+            }, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+
+            const { token: newToken } = response.data;
+            setToken(newToken);
+            setShowSuccessMessage(true);
+        } catch(err) {
+            setShowErrorMessage(true);
+        }
         // Send a request to the server to
         // update the user's info with any changes we've
         // made to the text input values
-        alert('Save functionality not implemented yet');
+        // alert('Save functionality not implemented yet');
     }
 
     const logOut = () => {
+        localStorage.removeItem('token');
+        history.push('/login');
         // We'll want to log the user out here
         // and send them to the "login page"
-        alert('Log out functionality not implemented yet');
+        // alert('Log out functionality not implemented yet');
     }
     
     const resetValues = () => {
+        setFavoriteFood(info.favoriteFood);
+        setHairColor(info.hairColor);
+        setBio(info.bio);
         // Reset the text input values to
         // their starting values (the data we loaded from the server)
-        alert('Reset functionality not implemented yet');
+        // alert('Reset functionality not implemented yet');
     }
     
     // And here we have the JSX for our component. It's pretty straightforward
     return (
         <div className="content-container">
-            <h1>Info for ______</h1>
+            <h1>Info for {email} </h1>
             {showSuccessMessage && <div className="success">Successfully saved user data!</div>}
             {showErrorMessage && <div className="fail">Uh oh... something went wrong and we couldn't save changes</div>}
             <label>
